dd4ebcdfff07cd77be01be09c98e68dfd60d60a5
vjagatap <77829130+vjagatap@users.noreply.github.com>
2021-11-06 18:43:21 +0530

    Add files via upload

---
+++ Task2.py
@@ -0,0 +1,17 @@
+a = "welcome*to#python&world!keep_learning^everyday"
+
+print(a[20])
+print(a[-16])
+print(a[21])
+print(a[-15])
+print(a[23])
+print(a[-13])
+print(a[31])
+print(a[-5])
+print(a[22])
+print(a[-14])
+print(a[30])
+print(a[-6])
+print(a[11])
+print(a[-25])
+
